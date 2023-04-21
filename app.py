489006159c837ec67dfb1310d5db8effa208b790
gefox00 <72073413+gefox00@users.noreply.github.com>
2023-04-21 14:13:38 +0900

    追記

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ st.write('https://charasheet.vampire-blood.net/')
 asset = st.session_state
 
 get_button = st.button(label='変換', key='-convert-')
+save_file = st.checkbox(label='ファイルに保存する')
+if save_file:
+    st.download_button(label='保存', key='-save-')
 # イベント処理
 if get_button and len(asset['-input-']) > 0:
     target = asset['-input-'] + '.js'
@@ -20,6 +23,7 @@ if get_button and len(asset['-input-']) > 0:
     if 'https://charasheet.vampire-blood.net' in target and target_json['game'] == 'nechro':
         convert = Nccatcher(target_json)
         clip = str(convert.ch_data_js).replace('\'', '"')
+
         st.write('変換結果を表示します以下の文字列をコピーしてココフォリアに張り付けてください')
         st.write(clip)
 
