4a0ff12710cab0c75aec109263bae9c92097cdd1
gefox00 <72073413+gefox00@users.noreply.github.com>
2023-04-25 09:48:31 +0900

    追記

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.write('https://charasheet.vampire-blood.net/')
 asset = st.session_state
 
 get_button = st.button(label='変換', key='-convert-')
-# save_file = st.checkbox(label='ファイルに保存する')
-# if save_file:
-#     st.download_button(label='保存', key='-save-')
+save_file = st.checkbox(label='ファイルに保存する')
+if save_file:
+    st.download_button(label='保存', key='-save-')
 # イベント処理
 if get_button and len(asset['-input-']) > 0:
     target = asset['-input-'] + '.js'
